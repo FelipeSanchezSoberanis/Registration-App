ec00c85305628080da7b3ff9d9f8ad29a73338c7
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-04-07 12:49:22 -0600

    Group participant to create reg event

--- backend/src/main/java/com/fiuady/registrationApp/services/RegistrationEventService.java
+++ backend/src/main/java/com/fiuady/registrationApp/services/RegistrationEventService.java
@@ -1,7 +1,10 @@
 package com.fiuady.registrationApp.services;
 
+import com.fiuady.registrationApp.entities.Group;
 import com.fiuady.registrationApp.entities.RegistrationEvent;
+import com.fiuady.registrationApp.entities.User;
 import com.fiuady.registrationApp.exceptions.GroupNotFoundException;
+import com.fiuady.registrationApp.exceptions.InsufficientPermissionsException;
 import com.fiuady.registrationApp.exceptions.RegistrationEventNameTakenException;
 import com.fiuady.registrationApp.repositories.GroupRepository;
 import com.fiuady.registrationApp.repositories.RegistrationEventRepository;
@@ -10,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RegistrationEventService {
@@ -21,9 +25,18 @@ public class RegistrationEventService {
     public RegistrationEvent createRegistrationEvent(
             Long groupId, RegistrationEvent registrationEvent) {
 
-        if (groupRepository.findById(groupId).isEmpty()) throw new GroupNotFoundException(groupId);
+        Optional<Group> groupOpt = groupRepository.findById(groupId);
+
+        if (groupOpt.isEmpty()) throw new GroupNotFoundException(groupId);
         if (registrationEventRepository.findByName(registrationEvent.getName()).isPresent())
             throw new RegistrationEventNameTakenException(registrationEvent.getName());
+
+        User loggedInUser = userService.getLoggedInUser();
+
+        if (!groupOpt.get().getOwner().equals(loggedInUser)
+                && !groupOpt.get().getParticipants().contains(loggedInUser))
+            throw new InsufficientPermissionsException(
+                    "You're not the owner or a participant of this group");
 
         registrationEvent.setOwner(userService.getLoggedInUser());
 
--- backend/src/main/java/com/fiuady/registrationApp/startup/InitialData.java
+++ backend/src/main/java/com/fiuady/registrationApp/startup/InitialData.java
@@ -1,8 +1,10 @@
 package com.fiuady.registrationApp.startup;
 
+import com.fiuady.registrationApp.entities.Group;
 import com.fiuady.registrationApp.entities.Permission;
 import com.fiuady.registrationApp.entities.Role;
 import com.fiuady.registrationApp.entities.User;
+import com.fiuady.registrationApp.repositories.GroupRepository;
 import com.fiuady.registrationApp.repositories.PermissionRepository;
 import com.fiuady.registrationApp.repositories.RoleRepository;
 import com.fiuady.registrationApp.repositories.UserRepository;
@@ -25,12 +27,32 @@ public class InitialData {
     @Autowired private UserRepository userRepo;
     @Autowired private RoleRepository roleRepo;
     @Autowired private PermissionRepository permRepo;
+    @Autowired private GroupRepository groupRepository;
 
     @EventListener(ApplicationReadyEvent.class)
     public void createInitialData() {
         createPermissions();
         createRoles();
         createUsers();
+        createGroups();
+    }
+
+    private void createGroups() {
+        User user1 = userRepo.findById(1L).get();
+        User user2 = userRepo.findById(2L).get();
+
+        Group group1 = new Group();
+        group1.setName("User's 1 group");
+        group1.setParticipants(Set.of(user2));
+        group1.setOwner(user1);
+
+        groupRepository.save(group1);
+
+        Group group2 = new Group();
+        group2.setName("User's 1 group 2");
+        group2.setOwner(user1);
+
+        groupRepository.save(group2);
     }
 
     private void createUsers() {
